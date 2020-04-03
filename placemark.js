f77d00fef018cd8836b96da1d522a9e5970156e5
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-04-03 10:05:57 +0300

    yandex map debug

--- placemark.js
+++ placemark.js
@@ -8,21 +8,6 @@ function init() {
             searchControlProvider: 'yandex#search'
         }),
 
-        // Создаем геообъект с типом геометрии "Точка".
-        myGeoObject = new ymaps.GeoObject({
-            // Описание геометрии.
-            geometry: {
-                type: "Point",
-                coordinates: [55.8, 37.8]
-            }
-        }, {
-            // Опции.
-            // Иконка метки будет растягиваться под размер ее содержимого.
-            preset: 'islands#blackStretchyIcon',
-            // Метку можно перемещать.
-            draggable: true
-        });
-
     myMap.geoObjects
         .add(new ymaps.Placemark([55.833436, 37.715175], {
             balloonContent: '<strong>серобуромалиновый</strong> цвет'
