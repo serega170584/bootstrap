c538034e3f1aa1094976f231a6fb16e95acbce4a
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-04-05 00:43:45 +0300

    heap algorithm

---
+++ heap.php
@@ -0,0 +1,22 @@
+<?php
+$a = [9, 1, 2, 4, 3, 5, 8, 6, 7];
+function heapify($a, $i)
+{
+    $leftIndex = 2 * $i + 1;
+    $rightIndex = 2 * $i + 2;
+    if ($a[$i] > $a[$leftIndex]) {
+        $res = $a[$i];
+        $a[$i] = $a[$leftIndex];
+        $a[$leftIndex] = $res;
+    }
+    if ($a[$i] > $a[$rightIndex]) {
+        $res = $a[$i];
+        $a[$i] = $a[$leftIndex];
+        $a[$leftIndex] = $res;
+    }
+    return $a;
+}
+
+for ($i = count($a) - 1; $i > 0; --$i) {
+    $a = heapify($a, $i);
+}
