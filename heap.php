4769a372fe6b36abbf4a1301131359295ac52e68
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-04-05 13:49:31 +0300

    heap algorithm

--- heap.php
+++ heap.php
@@ -1,23 +1,25 @@
 <?php
 $a = [9, 1, 2, 4, 3, 5, 8, 6, 7];
-function heapify($a, $i)
+function &heapify($a, $i)
 {
-    $leftIndex = 2 * $i + 1;
-    $rightIndex = 2 * $i + 2;
-    if ($a[$i] > $a[$leftIndex]) {
-        $res = $a[$i];
-        $a[$i] = $a[$leftIndex];
-        $a[$leftIndex] = $res;
-    }
-    if ($a[$i] > $a[$rightIndex]) {
-        $res = $a[$i];
-        $a[$i] = $a[$leftIndex];
-        $a[$leftIndex] = $res;
+    $isFinished = false;
+    while ($isFinished) {
+        $leftIndex = 2 * $i + 1;
+        $rightIndex = 2 * $i + 2;
+        $largeIndex = $i;
+        if ($leftIndex < count($a) && $a[$i] < $a[$leftIndex]) {
+            $largeIndex = $leftIndex;
+        }
+        if ($rightIndex > count($a) && $a[$i] < $a[$rightIndex]) {
+            $largeIndex = $rightIndex;
+        }
+        $a[$i] = $a[$largeIndex];
+        $isFinished = ($i == $largeIndex);
     }
     return $a;
 }
 
-for ($i = count($a) - 1; $i > 0; --$i) {
-    $a = heapify($a, ceil($i / 2) - 1);
+for ($i = ceil((count($a) - 1) / 2 - 1); $i >= 0; --$i) {
+    $a = heapify($a, $i);
 }
 var_dump($a);
