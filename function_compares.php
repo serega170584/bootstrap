ff2349e52231a6883f9f451e00c02155771ce965
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-04-13 23:47:24 +0300

    function compares

--- function_compares.php
+++ function_compares.php
@@ -5,5 +5,5 @@ $time = microtime(true);
 $c = $a + $b;
 var_dump(microtime(true) - $time);
 $time = microtime(true);
-$c = array_sum([$a, $b]);
+$c = array_sum([1, 2]);
 var_dump(microtime(true) - $time);
