2e89d905657b183ba77dfddc99feafa1e5d5ee43
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-04-13 23:50:38 +0300

    function compares

--- function_compares.php
+++ function_compares.php
@@ -2,7 +2,10 @@
 $a = 1;
 $b = 2;
 $time = microtime(true);
-$c = $a + $b;
+$c = 0;
+foreach ([1, 2] as $val) {
+    $c += $val;
+}
 var_dump(microtime(true) - $time);
 $time = microtime(true);
 $c = array_sum([1, 2]);
