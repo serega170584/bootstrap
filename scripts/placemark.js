d671b7342cda15d5b9e240f8116bc710a8c3e12e
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-04-20 10:25:59 +0300

    yandex map fix

--- scripts/placemark.js
+++ scripts/placemark.js
@@ -2,15 +2,15 @@ ymaps.ready(init);
 
 function init() {
     var myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
+        center: [55.81, 37.63],
         zoom: 10
     }, {
         searchControlProvider: 'yandex#search'
     });
 
     myMap.geoObjects
-        .add(new ymaps.Placemark([55.833436, 37.715175], {
-            balloonContent: '<strong>серобуромалиновый</strong> цвет'
+        .add(new ymaps.Placemark([55.812696, 37.633205], {
+            balloonContent: '<strong>Проспект Мира 101с1</strong>'
         }, {
             preset: 'islands#dotIcon',
             iconColor: '#735184'
